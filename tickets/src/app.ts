349245a77a6e35a36aff3ad092be7d403e6529a1
Brian Kiernan <brian@briankiernan.com>
2021-03-19 13:10:08 -0400

    add new "tickets" service

---
+++ tickets/src/app.ts
@@ -0,0 +1,22 @@
+import express, { json } from 'express';
+import 'express-async-errors';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError } from '@tclick/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+  })
+);
+
+app.all('*', () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };

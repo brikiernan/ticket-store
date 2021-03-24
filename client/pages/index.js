bdce4ab88e2c5205ede3894610cf8d5f8301e839
Brian Kiernan <brian@briankiernan.com>
2021-03-24 16:13:37 -0400

    update landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,15 @@
+import buildClient from '../api/build-client';
+
 const LandingPage = ({ currentUser }) => {
   return <h1>{currentUser ? 'You are signed in' : 'You are NOT signed in'}</h1>;
+};
+
+LandingPage.getInitialProps = async context => {
+  console.log('LANDING PAGE!');
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+
+  return data;
 };
 
 export default LandingPage;
